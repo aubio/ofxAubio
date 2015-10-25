76a9e76c86e358fcc194920e005ab62adc688da8
Paul Brossier <piem@piem.org>
2015-10-25 03:06:45 +0100

    scripts/fetch_aubio_osx_framework.sh: add script to fetch aubio.framework

---
+++ scripts/fetch_aubio_osx_framework.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+AUBIO_VERSION=0.4.2
+AUBIO_ZIPFRAM=aubio-$AUBIO_VERSION.darwin_framework.zip
+AUBIO_FRAMPATH=${AUBIO_ZIPFRAM%%.zip}
+
+set -e
+set -x
+
+rm -rf $AUBIO_ZIPFRAM example_aubioDemo/$AUBIO_FRAMPATH
+wget http://aubio.org/bin/$AUBIO_VERSION/$AUBIO_ZIPFRAM
+unzip -x $AUBIO_ZIPFRAM -d example_aubioDemo/
+open example_aubioDemo/$AUBIO_FRAMPATH
